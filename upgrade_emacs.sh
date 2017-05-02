e051faa1066a24d923c180d7b7deff0e6a088ac4
Philip Yang <philip.yang@databricks.com>
2017-05-01 23:12:12 -0700

    init productivity

--- upgrade_emacs.sh
+++ upgrade_emacs.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-base_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+_bsd_="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-(cd "${base_dir}"
- git submodule foreach 'git checkout master ; git pull; if [ -f Makefile ]; then make clean && make; fi'
-)
 
 os_name=$(uname -s | tr '[:upper:]' '[:lower:]')
-os_emacs_exec="${base_dir}/emacs.${os_name}"
+os_emacs_exec="${_bsd_}/emacs.${os_name}"
+echo "OS detected: ${os_name}"
 
 function update_darwin() {
-    # brew upgrade emacs || \
-    #     brew install emacs \
-	#          --with-imagemagick \
-	#          --with-librsvg \
-	#          --with-cocoa
-
-    # brew link --overwrite emacs
-    # brew linkapps emacs
-    # brew cleanup emacs
     # Cask has better support for cocoa
-    brew cask install emacs
+    brew cask install emacs || echo "Okay"
 
     if [ -f "${os_emacs_exec}" ]; then
         rm -f /usr/local/bin/emacs
@@ -35,11 +24,15 @@ function update_linux() {
 }
 
 case "${os_name}" in 
-    linux) update_darwin 
+    darwin) update_darwin 
            ;;
-    darwin) update_linux 
+    linux) update_linux 
             ;;
     \?)
         echo "the operating system ${os_name} is not supported"; exit
         ;;
 esac
+
+(cd "${_bsd_}"
+ git submodule foreach 'git checkout master ; git pull; if [ -f Makefile ]; then make clean && make; fi'
+)
